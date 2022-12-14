0328766367b815510128cd73dc7d6c3bd3d88058
diegocardoso <diego.sacramento@engdb.com.br>
2022-12-14 19:24:12 -0300

    implements interface Avenger

---
+++ api/src/main/kotlin/com/avengers/api/application/domain/avenger/AvengerRepository.kt
@@ -0,0 +1,10 @@
+package com.avengers.api.application.domain.avenger
+
+interface AvengerRepository {
+
+    fun getAvengers():List<Avenger>
+    fun getDetail(id:Long):Avenger
+    fun create(avenger: Avenger):Avenger
+    fun delete(id:Long)
+    fun update(avenger: Avenger):Avenger
+}
